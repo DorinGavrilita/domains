fbc22cdf99f8b955e6bcaae77cece2cdfe6522d8
DorinGavrilita <gavrilitadorin5@gmail.com>
2022-11-28 09:59:22 +0200

    lactis

--- Calculator/solution6/index.php
+++ Calculator/solution6/index.php
@@ -10,60 +10,10 @@
 </head>
 
 <body>
-	<main>
-		<!-- <h3>Calculator 6</h3> -->
-
-		<form class="container" method="POST">
-			<div class="display-top">
-				<div class="display-1">0</div>
-				<div class="display-2">0</div>
-				<div class="temp-result">0</div>
-			</div>
-			<section class="all_buttons" id="calculator-buttons">
-				<!--(Memory Clear) -стереть данные из ячейки памяти.-->
-				<button type="submit" name="sub" value="mc">MC</button>
-				<!--(Memory Read) -считать число из ячейки памяти и вывести его на дисплей.-->
-				<button type="submit" name="sub" value="mr">MR</button>
-				<!-- MS (Memory Save) -сохранить число, отображенное в данный момент на дисплее калькулятора в память.-->
-				<button type="submit" name="sub" value="ms">MS</button>
-				<button type="submit" name="sub" value="m+">M+</button> <!-- M+ - добавление числа которое на экране к памяти -->
-				<button type="submit" name="sub" value="m-">M-</button> <!-- М- - вычитание числа, которое на экране из памяти -->
-				<button type="submit" name="sub" value="(">(</button>
-				<button type="submit" name="sub" value=".">.</button>
-				<button type="submit" name="sub" value=")">)</button>
-				<button type="submit" name="sub" value="C">C</button>
-				<button class="all-clean" type="submit" name="sub" value="AC">AC</button>
-				<button type="submit" name="sub" value="7">7</button>
-				<button type="submit" name="sub" value="8">8</button>
-				<button type="submit" name="sub" value="9">9</button>
-				<button type="submit" name="sub" value="/">/</button>
-				<button type="submit" name="sub" value="abs">abs</button>
-				<button type="submit" name="sub" value="4">4</button>
-				<button type="submit" name="sub" value="5">5</button>
-				<button type="submit" name="sub" value="6">6</button>
-				<button type="submit" name="sub" value="*">*</button>
-				<button type="submit" name="sub" value="mod">mod</button>
-				<button type="submit" name="sub" value="1">1</button>
-				<button type="submit" name="sub" value="2">2</button>
-				<button type="submit" name="sub" value="3">3</button>
-				<button type="submit" name="sub" value="-">-</button>
-				<button type="submit" name="sub" value="+">+</button>
-				<button type="submit" name="sub" value="0">0</button>
-				<button type="submit" name="sub" value="cos">cos</button>
-				<button type="submit" name="sub" value="sin">sin</button>
-				<button type="submit" name="sub" value="+/-">+/-</button>
-				<button type="submit" name="sub" value="=">=</button>
-			</section>
-			<div class="display-bottom">
-				<div class="display-3">BIN: 0</div>
-				<div class="display-4">OCT: 0</div>
-				<div class="display-5">HEX: 0</div>
-			</div>
-		</form>
-	</main>
 
 	<?php
-	class Calcul
+	$num1 = ' ';
+	class Calculator
 	{
 		public $num1;
 		public $num2;
@@ -109,7 +59,67 @@
 			}
 		}
 	}
+
+	$num1 = $_POST['sub'];
+	echo $num1;
+
+	// $solution = new Calculator($num1,);
+
 	?>
+
+
+	<main>
+		<!-- <h3>Calculator 6</h3> -->
+
+		<form class="container" method="POST">
+			<div class="display-top">
+				<div class="display-1">0</div>
+				<div class="display-2">0</div>
+				<div class="temp-result"><?php echo $num1; ?></div>
+			</div>
+			<section class="all_buttons" id="calculator-buttons">
+				<!--(Memory Clear) -стереть данные из ячейки памяти.-->
+				<button type="button" name="sub" value="mc">MC</button>
+				<!--(Memory Read) -считать число из ячейки памяти и вывести его на дисплей.-->
+				<button type="button" name="sub" value="mr">MR</button>
+				<!-- MS (Memory Save) -сохранить число, отображенное в данный момент на дисплее калькулятора в память.-->
+				<button type="submit" name="sub" value="ms">MS</button>
+				<button type="button" name="sub" value="m+">M+</button> <!-- M+ - добавление числа которое на экране к памяти -->
+				<button type="button" name="sub" value="m-">M-</button> <!-- М- - вычитание числа, которое на экране из памяти -->
+				<button type="button" name="sub" value="(">(</button>
+				<button type="button" name="sub" value=".">.</button>
+				<button type="button" name="sub" value=")">)</button>
+				<button type="button" name="sub" value="C">C</button>
+				<button class="all-clean" type="button" name="sub" value="AC">AC</button>
+				<button type="button" name="sub" value="7">7</button>
+				<button type="button" name="sub" value="8">8</button>
+				<button type="button" name="sub" value="9">9</button>
+				<button type="button" name="sub" value="/">/</button>
+				<button type="button" name="sub" value="abs">abs</button>
+				<button type="button" name="sub" value="4">4</button>
+				<button type="button" name="sub" value="5">5</button>
+				<button type="button" name="sub" value="6">6</button>
+				<button type="button" name="sub" value="*">*</button>
+				<button type="button" name="sub" value="mod">mod</button>
+				<button type="button" name="sub" value="1">1</button>
+				<button type="button" name="sub" value="2">2</button>
+				<button type="button" name="sub" value="3">3</button>
+				<button type="button" name="sub" value="-">-</button>
+				<button type="button" name="sub" value="+">+</button>
+				<button type="button" name="sub" value="0">0</button>
+				<button type="button" name="sub" value="cos">cos</button>
+				<button type="button" name="sub" value="sin">sin</button>
+				<button type="button" name="sub" value="+/-">+/-</button>
+				<button type="button" name="sub" value="=">=</button>
+			</section>
+			<div class="display-bottom">
+				<div class="display-3">BIN: 0</div>
+				<div class="display-4">OCT: 0</div>
+				<div class="display-5">HEX: 0</div>
+			</div>
+		</form>
+	</main>
+
 
 </body>
 
